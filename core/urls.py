af153f523f29fd179c42c10050a7af65b370ec28
Nam Quoc Tran <namquoctran24@gmail.com>
2026-04-17 09:15:11 +0200

    feat: wire forum_app URLs under /api/

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('user_auth_app.urls')),
+    path('api/', include('forum_app.urls')),
 ]
---
+++ forum_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import (
+    CommentDetailView,
+    CommentListCreateView,
+    PostDetailView,
+    PostListCreateView,
+)
+
+urlpatterns = [
+    path('posts/', PostListCreateView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('comments/', CommentListCreateView.as_view(), name='comment-list'),
+    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
+]
