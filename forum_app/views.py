a41fb2a9f19219510cf5ee1a3f3076a6c94b6ca6
Nam Quoc Tran <namquoctran24@gmail.com>
2026-05-06 11:05:00 +0200

    feat(views): apply PostThrottle to post and comment list views

--- forum_app/views.py
+++ forum_app/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics, permissions
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 from .models import Comment, Post
 from .permissions import IsOwnerOrReadOnly
 from .serializers import CommentSerializer, PostSerializer
+from .throttling import PostThrottle
 
 
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    throttle_classes = [PostThrottle, AnonRateThrottle, UserRateThrottle]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -24,6 +27,7 @@ class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    throttle_classes = [PostThrottle, AnonRateThrottle, UserRateThrottle]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
