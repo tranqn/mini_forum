0bc3ce116d7de290147abfff48aaa3a9e575d302
Nam Quoc Tran <namquoctran24@gmail.com>
2026-05-07 09:34:00 +0200

    feat(throttling): limit PostThrottle to POST requests only

--- forum_app/throttling.py
+++ forum_app/throttling.py
@@ -3,3 +3,8 @@ from rest_framework.throttling import ScopedRateThrottle
 
 class PostThrottle(ScopedRateThrottle):
     scope = 'posts'
+
+    def allow_request(self, request, view):
+        if request.method != 'POST':
+            return True
+        return super().allow_request(request, view)
